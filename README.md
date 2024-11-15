cacace095fa205ca4a7a764ca7d3cae74427ac76
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-15 12:55:21 +0100

    Make small function to print movie release date
    From a small list of movies

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ruby-training
+# this is my first take on ruby programing
+# date 15-11-2024
---
+++ main.rb
@@ -0,0 +1,8 @@
+movies = {
+  :"back to the future" => 1985,
+  :"the princess bride" => 1987,
+  :"inception" => 2010,
+  :"the matrix" => 1999
+}
+
+puts movies[:"inception"]
