a93131727c1c17466baf73ef7a6a38fc042ae7a1
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-15 13:01:26 +0100

    Made array variable to take movie names and
    Store the movie years to the array

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 movies = {
-  :"back to the future" => 1985,
-  :"the princess bride" => 1987,
-  :"inception" => 2010,
-  :"the matrix" => 1999
+  :back_to_the_future => 1985,
+  :the_princess_bride => 1987,
+  :inception => 2010,
+  :the_matrix => 1999
 }
-
-puts movies[:"inception"]
+movie_years = [movies[:back_to_the_future], movies[:the_princess_bride], movies[:inception], movies[:the_matrix]]
+puts movie_years
