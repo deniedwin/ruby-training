b84834fa1beccdc2f66759b2f669578ca242e93a
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-15 16:25:01 +0100

    Make function to get user input and add number to

--- main.rb
+++ main.rb
@@ -1,18 +1,29 @@
-# hash to store movie names as key and release dates as value
-movies = {
-  :back_to_the_future => 1985,
-  :the_princess_bride => 1987,
-  :inception => 2010,
-  :the_matrix => 1999
-}
-# array to store the movie years of the above hash
-movie_years = [movies[:back_to_the_future], movies[:the_princess_bride], movies[:inception], movies[:the_matrix]]
+# # hash to store movie names as key and release dates as value
+# movies = {
+#   :back_to_the_future => 1985,
+#   :the_princess_bride => 1987,
+#   :inception => 2010,
+#   :the_matrix => 1999
+# }
+# # array to store the movie years of the above hash
+# movie_years = [movies[:back_to_the_future], movies[:the_princess_bride], movies[:inception], movies[:the_matrix]]
 # puts movie_years
+# puts "\n"
 
-# function to calcualte the facatorial reuslt of any number 7! = 5040
-my_var = 7
-result = 1
-for i in 1..my_var do
-  result = i*result
-end
-puts result
+# # function to calcualte the facatorial reuslt of any number 7! = 5040
+# my_var = 7
+# result = 1
+# for i in 1..my_var do
+#   result = i*result
+# end
+# puts result
+# puts "\n"
+
+# # function to take user name and outputs greeting message
+# name = gets.chomp
+# puts "hi " + name + " fin"
+
+# function to ask for user name and return what his age will be in x-years
+age = gets.chomp.to_i
+added_years = 10
+puts age + added_years
