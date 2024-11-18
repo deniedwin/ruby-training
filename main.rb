3cf5ccc691cba0a5ae01441a6539fdc3a9935617
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-18 15:31:34 +0100

    Installed pry-byebug

--- main.rb
+++ main.rb
@@ -112,11 +112,18 @@
 
 # function to check if passed string is isogram, that is a word that
 # has no repeating letter
-def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
-  unique_length = string_array.uniq.length
-  original_length == unique_length
-end
-puts isogram?("Odin")
-#=> false
+# def isogram?(string)
+#   original_length = string.length
+#   string_array = string.downcase.split("")
+#   p string_array
+#   unique_length = string_array.uniq.length
+#   p unique_length
+#   original_length == unique_length
+# end
+# puts isogram?("Odin")
+# #=> false
+
+puts "Using puts:"
+puts []
+p "Using p:"
+p []
