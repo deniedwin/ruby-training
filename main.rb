392258b511587ed8efc305b8c857eba4edf99264
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-24 14:49:54 +0100

    Iterate thru nested arrays hashes

--- main.rb
+++ main.rb
@@ -140,6 +140,42 @@
 
 # yell_greeting("bob")
 
-immutable = Array.new(3) { Array.new(2) }
-immutable[0][0] = 1000
-print immutable
+# # show how to create nested array that are immutable
+# immutable = Array.new(3) { Array.new(2) }
+# immutable[0][0] = 1000
+# print immutable
+
+# show how to iterate over multidimensional hashes
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla", "mint chip"],
+    knows: nil
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+
+contacts.each do |person, data|
+  #at this level, "person" is Jon Snow or Freddy and "data" is a hash of
+  #key/value pairs to iterate over the "data" hash, we can use the following
+  #line:
+
+  data.each do |attribute, value|
+    #at this level, "attribute" describes the key of :name, :email,
+    #:favorite_ice_cream_flavors, or :knows we need to first check and see if
+    #the key is :favorite_ice_cream_flavors, if it is, that means the VALUE is
+    #an array that we can iterate over to print out each element
+
+    if attribute == :favorite_ice_cream_flavors
+      value.each do |flavor|
+        # here, each index element in an ice cream flavor string
+        puts "#{flavor}"
+      end
+    end
+  end
+end
